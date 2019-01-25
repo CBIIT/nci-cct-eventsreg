faba3ec30a5cc2553622e6bb6aa8a1d228ae607a
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2019-01-25 12:18:52 -0500

    Cleaned up database - removed unnecessary files

--- database.sql
+++ database.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2217c5bd353d91da2b632453747ff5712ccc1120becf28ded3ca46cd72522a14
-size 215797312
+oid sha256:94a3b5ebe722175361f52a88607caabc24c6f16e9699afabe563e06e7171d442
+size 40803330
