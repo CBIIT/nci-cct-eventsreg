c66adbf700edd592adcf6f7a8c24c72e630fa1d5
Kneisler, Chris (NIH/NCI) [C] <chris.kneisler@nih.gov>
2019-02-28 13:08:20 -0500

    Synced with PROD

--- database.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b95ea88b138ca28744d3fc03c100517c39b4cca2b8af6518d37d776c574dbea6
-size 43193950
--- setup/sanitize.sh
+++ setup/sanitize.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# This script will test if you have given a leap year or not.
+
+echo "Type the admin user password:"
+
+read ADMIN_PASS
+
+echo "Type the test user password:"
+
+read TEST_PASS
 echo ""
 echo "IMPORT PRODUCTION DATABASE FIRST"
 echo "SYNTAX: drush sql-cli < backup-<date>.mysql"
@@ -23,7 +32,7 @@ drush webform:purge --all -y
 echo ""
 echo "*Clear Cache"
 echo "drush cr"
-echo "drush cr"
+drush cr
 
 echo ""
 echo "*** COMPLETED"
@@ -32,13 +41,21 @@ echo "*TODO: Manually change the password of users with the drush user-password 
 echo "Change PASSWORD for the folowing users:"
 echo ""
 echo "drush user-password admin <password>"
+drush user-password admin $ADMIN_PASS
 echo "drush user-password cct_user <password>"
+drush user-password cct_user $TEST_PASS
 echo "drush user-password nob_user <password>"
+drush user-password nob_user $TEST_PASS
 echo "drush user-password next_user <password>"
+drush user-password next_user $TEST_PASS
 
 echo ""
 echo "*TODO: dump database"
 echo "drush sql-dump > database.sql"
+drush sql-dump > ../database.sql
+cd ..
+zip database.zip database.sql
+rm database.sql
 echo ""
 
 echo "*TODO: Remove backup-<date>.mysql file"
