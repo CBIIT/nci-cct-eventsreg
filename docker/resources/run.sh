a1747771d4eab88cc5ef6057b635906f0922640d
regulator95 <seinolf@gmail.com>
2019-03-29 11:54:38 -0400

    checking in more docker stuff;

--- docker/resources/run.sh
+++ docker/resources/run.sh
@@ -1,9 +1,15 @@
 if [ -d "/local/drupal/site" ]; then
     echo "Skipping site download"
+    cd /local/drupal/site
+    echo "pulling latest code from $repository"
+    git pull
 else
-    echo "Pulling code from $repository"
+    echo "Cloning code from $repository"
     git clone $repository site
     cd site
     composer require drush/drush
 fi
+cp /tmp/settings.php /local/drupal/site/web/sites/default
+cp /tmp/.htaccess /local/drupal/site
+
 exec httpd -DFOREGROUND
