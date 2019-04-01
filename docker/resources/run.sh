fe450b7d00f1d3dcd9b0ab305ef6cd80027c0260
regulator95 <seinolf@gmail.com>
2019-04-01 14:05:18 -0400

    adding empty drupal install for no repo

--- docker/resources/run.sh
+++ docker/resources/run.sh
@@ -1,15 +1,22 @@
-if [ -d "/local/drupal/site" ]; then
-    echo "Skipping site download"
+if [[ -z $repository ]]; then
+    echo "Create new drupal site"
+    composer create-project drupal-composer/drupal-project:8.x-dev /local/drupal/site --no-interaction
     cd /local/drupal/site
-    echo "pulling latest code from $repository"
-    git pull
+    composer upgrade drupal/core:$DRUPAL_VERSION  --with-dependencie    
 else
-    echo "Cloning code from $repository"
-    git clone $repository site
-    cd site
-    composer require drush/drush
+    if [ -d "/local/drupal/site" ]; then
+        echo "Skipping site download"
+        cd /local/drupal/site
+        echo "pulling latest code from $repository"
+        git pull
+    else
+        echo "Cloning code from $repository"
+        git clone $repository site
+        cd site
+        composer require drush/drush
+    fi
+    cp /tmp/settings.php /local/drupal/site/web/sites/default
+    cp /tmp/.htaccess /local/drupal/site
 fi
-cp /tmp/settings.php /local/drupal/site/web/sites/default
-cp /tmp/.htaccess /local/drupal/site
 
 exec httpd -DFOREGROUND
