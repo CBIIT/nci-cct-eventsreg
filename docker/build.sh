663fa47704602bd633aa1b559c11e90fe5c402fc
Shaun Einolf <einolfs@mail.nih.gov>
2019-04-24 12:54:41 -0400

    re-creating build script to auto tag imgaes

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-docker kill drupal
-docker rm drupal
-docker rmi cbiit/drupal -f
-docker build -t cbiit/drupal .
-docker run -e repository=$repository -e hash_salt=$hash_salt -e database=$database -e username=$username -e password=$password -e host=$host -e port=$port -e namespace=$namespace -e driver=$driver -p 8080:80 --rm --name drupal cbiit/drupal
-docker exec -it drupal bash
